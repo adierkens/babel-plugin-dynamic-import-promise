0859e119de627c5060dc42a5968678fff756285b
Adam Dierkens <adam@dierkens.com>
2018-04-09 16:21:32 -0700

    Try it

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-export default function({types: t }) {
+import template from 'babel-template';
+import syntax from 'babel-plugin-syntax-dynamic-import';
+
+const buildImport = template(`
+    (Promise.resolve(require(SOURCE)))
+`);
+
+export default function() {
   return {
+    inherits: syntax,
+
     visitor: {
+        Import(path) {
+            path.parentPath.replaceWith(buildImport({
+                SOURCE: path.parentPath.node.arguments
+            }));
+        }
     }
   };
 }
